bdec8213d6c04ba23922f2c00823e9d62b16de32
ksimpac <adasl0376210456789@gmail.com>
2021-03-19 16:56:12 +0800

    disable register,verify,reset route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['register' => false, 'verify' => false, 'reset' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
